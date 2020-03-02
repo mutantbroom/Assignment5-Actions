0d7416e5f219af5ea356aa8793b473f59d9c7026
mutantbroom <jon.beck@live.com>
2020-03-01 19:33:59 -0800

    Update task.py

--- task.py
+++ task.py
@@ -13,3 +13,8 @@ def circleArea(radius):
 def check_circleArea(result):
     result = circleArea(3)
     assert(result == 28.27)
+    
+
+def listGet(list1):
+    elements = [ list1[0], list1[-1] ]
+    return elements
